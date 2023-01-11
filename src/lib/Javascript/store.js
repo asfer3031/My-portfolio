dd9651fe1f08518e0159bed6502af15bede50abd
asfer3031 <asfer3031@gmail.com>
2023-01-11 14:49:24 -0600

    changed Javascript images path

--- src/lib/Javascript/store.js
+++ src/lib/Javascript/store.js
@@ -9,21 +9,21 @@ export const activeLi = '';
 export let projects = [
     {
         id: 'Picture Fetcher',
-        picture: 'src/lib/assets/images/photography2.jpg',
+        picture: '../src/lib/assets/images/photography2.jpg',
         description: "A project to view images of high quality from Unsplash free developer's api." +
             "This project demonsttrates how we can fetch data from a database that is not local.",
         url: '/Projects/Picture-Fetcher',
         Cardfootertitle: 'A huge thanks to Unsplash.',
-        cardfooterimageUrl: 'src/lib/assets/images/unsplash.png'
+        cardfooterimageUrl: '../src/lib/assets/images/unsplash.png'
     },
     {
         id: 'Polls Maker',
-        picture: 'src/lib/assets/images/polls.jpg',
+        picture: '../src/lib/assets/images/polls.jpg',
         description: 'A project to create polls.' +
             ' I was able to build this project following a youtube tutorial done By The Net Ninja.',
         url: '/Projects/Polls',
         Cardfootertitle: 'A huge thanks to The Net Ninja.',
-        cardfooterimageUrl: 'src/lib/assets/images/unnamed.jpg'
+        cardfooterimageUrl: '../src/lib/assets/images/unnamed.jpg'
     },
 
 ];
